7bba48e2ce2263b1465f123002fde1f0738361af
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-02 11:44:39 +0200

    [add]: chatwoot widget addition

---
+++ src/pages/chatwoot-script.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+declare global {
+    interface Window {
+        chatwootSDK: any;
+    }
+}
+
+const ChatwootScript = () => {
+    useEffect(() => {
+        const BASE_URL = 'https://app.chatwoot.com';
+        const script = document.createElement('script');
+        script.src = BASE_URL + '/packs/js/sdk.js';
+        script.defer = true;
+        script.async = true;
+        document.body.appendChild(script);
+
+        script.onload = function () {
+            window.chatwootSDK.run({
+                websiteToken: 'NT2gLHe68ZSLZKkjTnSAEc77',
+                baseUrl: BASE_URL,
+            });
+        };
+    }, []);
+
+    return null;
+};
+
+export default ChatwootScript;
